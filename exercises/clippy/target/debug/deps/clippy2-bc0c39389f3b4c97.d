92560103d13b5d92faac4556deb74368e56f9676
yaoliu <liuyao@163.com>
2021-12-11 23:22:46 +0800

    update rustlings

---
+++ exercises/clippy/target/debug/deps/clippy2-bc0c39389f3b4c97.d
@@ -0,0 +1,7 @@
+/Users/liuyao/rustlings/exercises/clippy/target/debug/deps/clippy2-bc0c39389f3b4c97.rmeta: clippy2.rs
+
+/Users/liuyao/rustlings/exercises/clippy/target/debug/deps/clippy2-bc0c39389f3b4c97.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
---
+++ exercises/quiz4.rs
@@ -0,0 +1,36 @@
+// quiz4.rs
+// This quiz covers the sections:
+// - Modules
+// - Macros
+
+// Write a macro that passes the quiz! No hints this time, you can do it!
+
+
+
+#[macro_export]
+macro_rules! my_macro {
+   ( $( $x:expr ),* ) => {
+       {
+           $(
+           let tmp = String::from("Hello ");
+           tmp.as_str().to_owned() + $x
+           )*
+       }
+   }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_my_macro_world() {
+        assert_eq!(my_macro!("world!"), "Hello world!");
+    }
+
+    #[test]
+    fn test_my_macro_goodbye() {
+        assert_eq!(my_macro!("goodbye!"), "Hello goodbye!");
+    }
+}
